88369902e3d1dacf40e9a3047c90b108b3b2db7e
Carlos Fernandez <krlosafernandez@gmail.com>
2022-11-14 00:40:18 -0500

    inicio clase 1, semana 3

--- app/src/main/java/com/example/ejemplo1ciclo4bg11/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1ciclo4bg11/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.ejemplo1ciclo4bg11
 
-import android.content.DialogInterface
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         edtUsername = findViewById(R.id.edtUsername)
         edtPassword = findViewById(R.id.edtPassword)
     }
@@ -25,43 +25,25 @@ class MainActivity : AppCompatActivity() {
         val messagepassword=getString(R.string.messagepassword)
         val messageusername=getString(R.string.messageusername)
         var username : String=edtUsername!!.text.toString()
-        if (username=="luis@correo.co")
-        {
+        if (username=="luis@correo.co") {
             if (edtPassword!!.text.toString()=="12345")
             {
-                val NegativeButton={_:DialogInterface,_:Int->}
-                val positiveButton={dialog:DialogInterface,which:Int->
-                    val intent = Intent(this,WelcomeActivity::class.java)
-                    startActivity(intent)
-                }
-
-                val dialog=AlertDialog.Builder(this)
-                    .setTitle("Welcome")
-                    .setMessage("User: "+username)
-                    .setPositiveButton("ok",positiveButton)
-                    .setNegativeButton("Cancelar",NegativeButton)
-                    .create()
-                    .show()
+                val intent = Intent(this,WelcomeActivity::class.java)
+                startActivity(intent)
             }
             else
             {
-                val dialog=AlertDialog.Builder(this)
-                    .setTitle("ERROR")
-                    .setMessage(messagepassword)
-                    .create()
-                    .show()
+                Toast.makeText(this,messagepassword,Toast.LENGTH_LONG).show()
             }
         }
         else
         {
-            /*
-            val dialog=AlertDialog.Builder(this)
-                .setTitle("ERROR")
-                .setMessage(messageusername)
-                .create()
-                .show()
-             */
             Toast.makeText(this,messageusername,Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun onLogin2(view: View) {
+        val intent = Intent(this,WelcomeActivity::class.java)
+        startActivity(intent)
     }
 }
